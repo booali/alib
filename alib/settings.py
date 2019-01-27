63887fbfe51eaafdf37b902969db088cfbdc20b6
ali bayat <booali767@gmail.com>
2019-01-27 21:43:45 +0330

    adding template

--- alib/settings.py
+++ alib/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+Templates_DIR = os.path.join(BASE_DIR,"template")
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'base',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'alib.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [Templates_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
